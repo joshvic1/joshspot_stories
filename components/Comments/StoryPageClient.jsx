b7b5ec494df05bdc78935739c3e31055dd20005e
[Olatunbosun Victor] <[olatunbosunvictor1@gmail.com]>
2025-11-13 12:22:58 +0100

    updated page

--- components/Comments/StoryPageClient.jsx
+++ components/Comments/StoryPageClient.jsx
@@ -56,6 +56,22 @@ export default function StoryPageClient({ story }) {
             <ShareIcon story={story} />
           </div>
 
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1759270581850188"
+            crossorigin="anonymous"
+          ></script>
+
+          <ins
+            class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-1759270581850188"
+            data-ad-slot="6027685473"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+
           <hr />
 
           <h3>Comments</h3>
