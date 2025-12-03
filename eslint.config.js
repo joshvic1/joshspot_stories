d42f1111afa163183069e92762424cb7de1339d6
[Olatunbosun Victor] <[olatunbosunvictor1@gmail.com]>
2025-12-03 16:58:09 +0100

    updated page

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,12 @@
+// eslint.config.js
+
+import next from "eslint-plugin-next";
+
 export default [
   {
+    plugins: {
+      next,
+    },
     rules: {
       "react/no-unescaped-entities": "off",
     },
