1282fc077dcb3e2a998c98de12d46c6fba940ad6
[Olatunbosun Victor] <[olatunbosunvictor1@gmail.com]>
2026-02-15 09:05:53 +0100

    adde adsense

--- components/OneSignalInit.js
+++ components/OneSignalInit.js
@@ -8,8 +8,19 @@ export default function OneSignalInit() {
       window.OneSignalDeferred.push(async function (OneSignal) {
         await OneSignal.init({
           appId: "258b3939-f36a-47ea-b9b0-926192f8cf39",
+
           notifyButton: {
             enable: true,
+            position: "bottom-left",
+          },
+
+          promptOptions: {
+            slidedown: {
+              enabled: true,
+              autoPrompt: true,
+              timeDelay: 5, // seconds after page load
+              pageViews: 1,
+            },
           },
         });
       });
